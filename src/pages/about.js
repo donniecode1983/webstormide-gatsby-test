a39718a9084430d1588b6e90a366be2cd6aaaa4f
donovanmurphy <don@murphywebconsulting.us>
2018-12-14 16:31:05 -0600

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/headr'
+
+export default () => (
+    <div
+    style={{color: 'teal'}}>
+        <Header/>
+        <p>Such WOW. Very React</p>
+    </div>
+)
