53a350aed4efd35523524c34a2bad188cd60e1ed
Donovan J Murphy <39827226+donniecode1983@users.noreply.github.com>
2018-12-14 18:53:08 -0600

    # Commit to new Branch Gatsby-Image-Work ## Working with gatsby image plugin

---
+++ src/pages/image-work.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from '../components/container'
+import Header from '../components/headr'
+
+export default () => (
+    <Container>
+        <Header
+            HeaderText={"This is a plugin to work with Images"}
+        />
+    </Container>
+
+)
